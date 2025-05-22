ab5403f21aa90f2b3fd64c4eadc295080d603804
mkalme <mikelis.kalme@gmail.com>
2025-05-22 15:19:13 +0300

    Add project files.

---
+++ StockSimulation/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockSimulation
+{
+    class Program
+    {
+        static double startingValue = 100;
+        static double movingByValue = 0.3;
+        static int averageTrendLength = 10;
+        static int averageSmallTrendLength = 100;
+
+        static int numberOfValues = 20000;
+
+        static double[] allValues = new double[numberOfValues];
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            allValues[0] = startingValue;
+
+            Random rand = new Random();
+
+            int percentByPositiveTrend = 0;
+
+            int trend = 1; //if number is positive than trend is positive and vice versa
+            int percentage = 10; //by how much percentage the value is likely to fallow the trend
+
+            int tempPercent = percentage;
+            int variationsByPercent = 0;
+            tempPercent -= variationsByPercent / 2;
+
+            int[,] range = { {0, 99}, {100, 199 + tempPercent} }; // second value is the percentage
+
+            int trendLength = rand.Next(averageTrendLength) + (int)(averageSmallTrendLength * 0.5);
+            if (rand.Next(2) == 0)
+            {
+                trend = -1;
+            }else {
+                trend = 1;
+
+                tempPercent += percentByPositiveTrend;
+            }
+
+            int tempTrend = 0;
+
+            string text = allValues[0].ToString() + "\n";
+
+            for (int i = 1; i < numberOfValues; i++) {
+                int randomValue = rand.Next((range[1, 1]) + (rand.Next(variationsByPercent + 1)) + 1);
+
+                if (randomValue >= range[1, 0])
+                {
+                    allValues[i] = allValues[i - 1] + (movingByValue * trend);
+                }else {
+                    allValues[i] = allValues[i - 1] + (movingByValue * (trend * -1));
+                }
+
+                //trend
+                tempTrend++;
+                if (tempTrend > trendLength) {
+                    tempTrend = 0;
+
+                    trendLength = rand.Next(averageTrendLength) + (int)(averageSmallTrendLength * 0.5);
+                    if (rand.Next(2) == 0)
+                    {
+                        trend = -1;
+                        tempPercent = percentage - variationsByPercent / 2;
+
+                        range[1, 1] = 199 + tempPercent;
+                    } else
+                    {
+                        trend = 1;
+                        tempPercent = percentage - variationsByPercent / 2;
+                        tempPercent += percentByPositiveTrend;
+
+                        range[1, 1] = 199 + tempPercent;
+                    }
+                }
+
+                if (i % 5 == 0) {
+                    text += allValues[i] + "\n";
+                }
+            }
+
+            Clipboard.SetText(text);
+            Console.WriteLine("Done");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StockSimulation/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StockSimulation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StockSimulation")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba2c4d12-b7ca-4483-a342-fe4285f7408d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
